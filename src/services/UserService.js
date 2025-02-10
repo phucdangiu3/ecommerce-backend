7eb75a80719baf6d7500b8b9e157a48f6bb06005
Phuc dep zai <phanhoangphuc2906@gmail.com>
2025-02-10 11:08:01 +0700

    Replace bcrypt with bcryptjs

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,5 @@
 const User = require("../models/UserModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt.js");
 const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
 
 const createUser = (newUser) => {
